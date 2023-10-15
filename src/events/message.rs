3650a37e09a50e278da0507f5537872fd84e178b
waki285 <suzuneu@suzuneu.com>
2023-10-15 04:26:48 +0200

    fix bug

--- src/events/message.rs
+++ src/events/message.rs
@@ -7,7 +7,7 @@ use serenity::{
 
 use tokio::time::sleep;
 
-use crate::{data::SharedData, constants::HTQ_CHANNEL_ID};
+use crate::{data::SharedData, constants::{HTQ_CHANNEL_ID, MENTION_REGEX}};
 
 pub async fn message(ctx: &Context, message: &Message, data: &SharedData) {
     let channel = message.channel(&ctx).await.unwrap().guild().unwrap();
@@ -22,20 +22,26 @@ pub async fn message(ctx: &Context, message: &Message, data: &SharedData) {
     }
 
     // Mentioned
-    if message.mentions_me(&ctx).await.unwrap() {
+    if MENTION_REGEX.is_match(&message.content) {
         if message.referenced_message.is_none() {
+            log::debug!("Mentioned");
             let mut mentioned = data.mentioned.lock().await;
             let time = mentioned.get(&message.author.id);
             if let Some(time) = time {
                 if time + 60000 > message.timestamp.timestamp_millis() as u64 {
                     sleep(Duration::from_secs(1)).await;
-                    message.reply(&ctx, format!("{}, How to Quote: <#{}>", message.author.id.get(), HTQ_CHANNEL_ID.get())).await.unwrap();
+                    message.reply(&ctx, format!("<@{}>, How to Quote: <#{}>", message.author.id.get(), HTQ_CHANNEL_ID.get())).await.unwrap();
                 } else {
                     mentioned.insert(
                         message.author.id,
                         message.timestamp.timestamp_millis() as u64,
                     );
                 }
+            } else {
+                mentioned.insert(
+                    message.author.id,
+                    message.timestamp.timestamp_millis() as u64,
+                );
             }
         }
     }
